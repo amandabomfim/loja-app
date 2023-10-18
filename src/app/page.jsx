e4109a2ae7157b3e1913fdc06a34a89ef01ad308
amandabomfim <rm99741@fiap.com.br>
2023-10-18 10:03:40 -0300

    Configurado rotas, e Slug.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-   <>
-    <h1>HOME</h1>
-   </>
+    <>
+
+
+      <h1>HOME</h1>
+
+
+    </>
   )
 }
---
+++ src/app/produtos/tenis/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Camisa() {
+  return (
+    <div>
+        <h1>Tênis</h1>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, dolorum voluptatem. Rem dolor quia facilis repudiandae nobis voluptatem vitae? Cumque quo ullam dolorum fugiat est officia sint quis. Fugit, modi?</p>
+            <figure>
+                <Image
+                    src="https://prints.ultracoloringpages.com/1c5c5fe9732b62308475c45956979053.png"
+                    alt="Tênis"
+                    width={300}
+                    height={300}
+                    />
+            </figure>
+            <p><Link href="/">Voltar...</Link></p>
+        </div>
+    </div>
+  )
+}
