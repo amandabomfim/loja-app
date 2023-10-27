8ce7c0dc2388032dcd24923f0f4e8d038509b075
amandabomfim <rm99741@fiap.com.br>
2023-10-27 11:24:04 -0300

    Criando GET-ALL e GET-ID na API e realizando o consumo nas views.Configurando novos hosts de imagens externos.

--- next.config.js
+++ next.config.js
@@ -3,11 +3,28 @@ const nextConfig = {}
 
 module.exports = {
     images: {
-        remotePatterns:[{
+        remotePatterns:[
+            {
             protocol: "https",
             hostname: "prints.ultracoloringpages.com",
             port: "",
             pathname: "/**"
-        }],
+        }
+        ,
+        {
+            protocol: "https",
+            hostname: "http.cat",
+            port: "",
+            pathname: "/images/**"
+        }
+        ,
+        {
+            protocol: "https",
+            hostname: "70096.cdn.simplo7.net",
+            port: "",
+            pathname: "/static/**"
+        }
+        ,
+    ],
     },
 }
