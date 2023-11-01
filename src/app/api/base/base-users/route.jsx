821d39336a6482a649df005f0ac37c957412b552
amandabomfim <rm99741@fiap.com.br>
2023-11-01 08:58:39 -0300

    Get All e Get-By-Id

--- src/app/api/base/base-users/route.jsx
+++
@@ -1,7 +0,0 @@
-import {promise as fs} from "fs";
-import { NextResponse } from "next/server";
-
-export async function GET(request,{params}) {
-  const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
-  return NextResponse.json(JSON.parse(file));
-}
